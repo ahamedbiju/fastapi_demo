233411a5afdf9e07a8a8d77b451648e1c3a4d169
Aadhil Ahammed Biju <baadhil2002@gmail.com>
2026-09-19 19:34:30 +0530

    Fix Railway DATABASE_URL driver

--- alembic/env.py
+++ alembic/env.py
@@ -11,13 +11,14 @@ from db_models import Base
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option(
-    "sqlalchemy.url",
-    os.getenv(
-        "DATABASE_URL",
-        config.get_main_option("sqlalchemy.url")
-    )
-)
+db_url = os.getenv("DATABASE_URL", config.get_main_option("sqlalchemy.url"))
+
+if db_url.startswith("postgres://"):
+    db_url = db_url.replace("postgres://", "postgresql+psycopg://", 1)
+elif db_url.startswith("postgresql://"):
+    db_url = db_url.replace("postgresql://", "postgresql+psycopg://", 1)
+
+config.set_main_option("sqlalchemy.url", db_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
